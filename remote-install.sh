4ffcfe8d560e73343d823de7abb3c9c5179b73fb
Adam Harris <32979919+oco-adam@users.noreply.github.com>
2024-03-18 07:41:16 +0100

    Update remote-install.sh

--- remote-install.sh
+++ remote-install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 BRANCH=main
-DOTFILES=~/repos/github.com/ostera/dotfiles
+DOTFILES=~/repos/github.com/oco-adam/dotfiles
 
 echo "Creating dotfiles path..."
 mkdir -p ${DOTFILES}
 cd ${DOTFILES}
 
 echo "Downloading dotfiles..."
-curl https://codeload.github.com/ostera/dotfiles/tar.gz/${BRANCH} > ${BRANCH}.tar.gz
+curl https://codeload.github.com/oco-adam/dotfiles/tar.gz/${BRANCH} > ${BRANCH}.tar.gz
 
 echo "Extracting..."
 tar -xf ${BRANCH}.tar.gz
